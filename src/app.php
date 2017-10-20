eb51de83a31d03cbb6d6138c5e1fbfeb8636bfb8
VladPolynko <vlad.polynko@gmail.com>
2017-10-20 12:12:44 +0300

    change structures and refactoring, sharing of responsibility

--- src/app.php
+++ src/app.php
@@ -1,12 +1,11 @@
 <?php
 use Symfony\Component\Routing;
-use first\src\calendar\controller\LeapYearController;
 
 $routes = new Routing\RouteCollection();
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => array(new LeapYearController(), 'indexAction'),
+    '_controller' => 'Calendar\Controller\LeapYearController::indexAction',
 )));
 
 return $routes;
